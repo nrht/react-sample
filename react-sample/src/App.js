2d41559d580205e0d9d804f1dc2b119264beeb70
nrht <i.norihi.472@gmail.com>
2021-01-24 22:22:11 +0900

    learn props

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,39 +1,28 @@
-import react from "react";
-
-// function App() {
-//   // const greeting = "Hi, Tom!";
-//   // const dom = <h1 className="foo">{greeting}</h1>
-//   // return dom;
-//   return (
-//     <react.Fragment>
-//       <label htmlFor="bar">Bar</label>
-//       <input type="text" onChange={() => {console.log("I'm clicked!")}} />
-//     </react.Fragment>
-//   );
-// }
-
-// import react from "react";
-
-// function App() {
-//   return react.createElement(
-//     "div",
-//     null,
-//     "Hello, world!"
-//   );
-// }
-
 const App = () => {
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5},
+    {name: "Noname"},
+  ]
+
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return  <User name={profile.neme} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
+
 }
-const Cat = () => {
-  return <div>Meow!</div>
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
